63aacdca2bc03b0465b7e10d21568a375b789b27
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 18:38:25 +0000

    Solutions

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
-//sidebar menu open
-document.getElementById("sidebar-menu-closed").addEventListener("click", () => {
+//sidebar
+document.getElementById("sidebar-menu-open").addEventListener("click", () => {
   let sidebar = document.getElementById("sidebar");
-  let content = document.getElementById("sidebar");
 
   sidebar.classList.add("open");
   sidebar.classList.remove("closed");
 });
 
-document.getElementById("sidebar-menu-open").addEventListener("click", () => {
+document.getElementById("sidebar-menu-close").addEventListener("click", () => {
   let sidebar = document.getElementById("sidebar");
-  let content = document.getElementById("sidebar");
 
   sidebar.classList.remove("open");
   sidebar.classList.add("closed");
 });
 
-function collapseSidebar(sidebar) {
-  let transition = content.style.transition;
-  sidebar.style.transition = "";
+//solutions area
+document.getElementById("solutions-area-open").addEventListener("click", () => {
+  let solutionsArea = document.getElementById("solutions-area");
+  let solutions = document.getElementById("solutions")
+  
+  solutionsArea.classList.add("showing");
 
-  requestAnimationFrame(function () {
-    sidebar.style.width = screenWidth - 160 + "px";
-    sidebar.style.transition = transition;
-  });
 
-  requestAnimationFrame(function () {
-    sidebar.style.width = 80 + "px";
-  });
+})
+
+document.getElementById("solutions-area-close").addEventListener("click", () => {
+  let solutionsArea = document.getElementById("solutions-area");
+  let solutions = document.getElementById("solutions")
+
+  solutionsArea.classList.remove("showing");
+})
+
+
+//solution
+let solutions = document.getElementsByClassName("solution")
+
+for(let i = 0; i < solutions.length; i++){
+  solutions[i].addEventListener("click", () => {
+    if(document.getElementById("solutions-area").classList.contains("showing")){
+      let children = solutions[i].children
+      for(let j = 0; j < children.length; j++){
+        if(children[j].id == "info"){
+          if(children[j].classList.contains("open")){
+            children[j].classList.remove("open")
+          }else{
+            children[j].classList.add("open")
+          }
+
+        }
+      }
+    }
+  })
 }
 
-function openSidebar(sidebar) {
-  let transition = sidebar.style.transition;
-  sidebar.style.transition = "";
 
-  requestAnimationFrame(function () {
-    sidebar.style.width = 0 + "px";
-    sidebar.style.minWidth = 0 + "px";
-    sidebar.style.transition = transition;
-  });
 
-  requestAnimationFrame(function () {
-    sidebar.style.width = screenWidth - 160 + "px";
-  });
-
-  sidebar.addEventListener("transitionend", function (e) {
-    sidebar.removeEventListener("transitionend", arguments.callee);
-  });
-
-  requestAnimationFrame(function () {
-    sidebar.style.width = null;
-  });
-}
+console.log(window.innerWidth)
