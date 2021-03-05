96cc7beb4c2604b45afd86d6341d9981b7f438e6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-05 17:20:46 +0000

    Tutoriais - missing search options

--- script.js
+++ script.js
@@ -18,21 +18,16 @@ function loadHome() {
 
   //solutions area
   document
-    .getElementById("solutions-area-open")
+    .getElementById("solutions-area-toggle")
     .addEventListener("click", () => {
       let solutionsArea = document.getElementById("solutions-area");
       let solutions = document.getElementById("solutions");
 
-      solutionsArea.classList.add("showing");
-    });
-
-  document
-    .getElementById("solutions-area-close")
-    .addEventListener("click", () => {
-      let solutionsArea = document.getElementById("solutions-area");
-      let solutions = document.getElementById("solutions");
-
-      solutionsArea.classList.remove("showing");
+      if (solutionsArea.classList.contains("showing")) {
+        solutionsArea.classList.remove("showing");
+      } else {
+        solutionsArea.classList.add("showing");
+      }
     });
 
   //solution
@@ -52,6 +47,109 @@ function loadHome() {
               children[j].classList.add("open");
             }
           }
+        }
+      }
+    });
+  }
+
+  //tutorials area show
+  document
+    .getElementById("tutorials-area-toggle")
+    .addEventListener("click", () => {
+      let tutorialsArea = document.getElementById("tutorials-area");
+      let tutorialsDiv = document.getElementById("tutorials");
+      let button = document.getElementById("tutorials-show-more-toggle");
+
+      if (tutorialsArea.classList.contains("showing")) {
+        if (tutorialsDiv.classList.contains("showing")) {
+          setTimeout(() => {
+            tutorialsArea.classList.remove("showing");
+          }, 1000);
+
+          tutorialsDiv.classList.remove("showing");
+          button.classList.remove("showing");
+          button.innerHTML = "Ver mais";
+        } else {
+          tutorialsArea.classList.remove("showing");
+        }
+      } else {
+        tutorialsArea.classList.add("showing");
+      }
+    });
+
+  //tutorials area show more
+  document
+    .getElementById("tutorials-show-more-toggle")
+    .addEventListener("click", () => {
+      let tutorialsArea = document.getElementById("tutorials-area");
+      let tutorialsDiv = document.getElementById("tutorials");
+      let button = document.getElementById("tutorials-show-more-toggle");
+
+      if (tutorialsArea.classList.contains("showing")) {
+        if (tutorialsDiv.classList.contains("showing")) {
+          window.location.href = "#";
+        } else {
+          tutorialsDiv.classList.add("showing");
+          button.classList.add("showing");
+          button.innerHTML = "Ver tudo";
+        }
+      }
+    });
+
+  //tutorial
+  let tutorials = document.getElementsByClassName("tutorial");
+
+  //playing video
+  for (let i = 0; i < tutorials.length; i++) {
+    tutorials[i]
+      .querySelector(".box >.wrapper >.play >img")
+      .addEventListener("click", () => {
+        if (
+          document
+            .getElementById("tutorials-area")
+            .classList.contains("showing")
+        ) {
+          let video = tutorials[i]
+            .querySelector(".box")
+            .children.namedItem("video");
+          if (tutorials[i].classList.contains("playing")) {
+            video.pause();
+
+            tutorials[i].classList.remove("playing");
+          } else {
+            tutorials[i].classList.add("playing");
+            video.play();
+          }
+        }
+      });
+  }
+
+  //fullscreen
+  for (let i = 0; i < tutorials.length; i++) {
+    tutorials[i].querySelector(".fullscreen").addEventListener("click", () => {
+      console.log("clic");
+      if (
+        document.getElementById("tutorials-area").classList.contains("showing")
+      ) {
+        console.log("hes");
+        let box = tutorials[i].querySelector(".box");
+        if (tutorials[i].classList.contains("fullscreen")) {
+          box.style.height = null;
+
+          tutorials[i].classList.remove("fullscreen");
+        } else {
+          var elementTransition = box.style.transition;
+          box.style.transition = "";
+
+          requestAnimationFrame(function () {
+            box.style.height = 0 + "px";
+            box.style.transition = elementTransition;
+
+            requestAnimationFrame(function () {
+              box.style.height = window.innerHeight + "px";
+            });
+          });
+          tutorials[i].classList.add("fullscreen");
         }
       }
     });
